401c162c1b06e72e3b23f8ec6a8005124f556999
sakibcse11 <sakib.cse.11@gmail.com>
2025-04-23 11:56:16 +0600

    Initial project setup with users app

---
+++ config/settings/local.py
@@ -0,0 +1,15 @@
+from .base import *
+DEBUG = True
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+# Debug toolbar settings
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
